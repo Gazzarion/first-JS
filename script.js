59c0a44389fcc5deefd48faa33ecb825d79bae7f
Alex <koloboka816@gmail.com>
2020-04-19 22:32:16 +0300

    First Js

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let money = prompt('Your budget for month?', ""),
+    time = prompt('Enter your date in format YYYY-MM-DD', ""),
+    appData = {
+        money: 100,
+        time: "timeData",
+        expenses: {
+            "answer at first question" : "answer at second question"
+        },
+        optionalExpenses: "",
+        income: [],
+        savings: false,
+    };
+    prompt('Enter a required cost item this month', "" ),
+    prompt('How much will it cost?', "" );
+    alert(dd)
